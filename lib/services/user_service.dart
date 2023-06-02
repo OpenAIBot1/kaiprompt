beafc8878e3296083f43db908695fd74e27e6eea
Aleksandr Podgaiko <aleksandrpodgaiko@Aleksandrs-MacBook-Pro.local>
2023-06-02 14:52:41 +0800

    user service updated

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -19,6 +19,52 @@ class UserService {
   Future<void> updateUser(UserModel user) async {
     await _firestore.collection('Users').doc(user.userId).update(user.toMap());
   }
+
+  Future<void> deleteUser(String userId) async {
+    await _firestore.collection('Users').doc(userId).delete();
+  }
+
+  Future<void> upvotePrompt(String userId, String promptId) async {
+    await _firestore.collection('Users').doc(userId).update({
+      'upvoted_prompts.$promptId': Timestamp.now(),
+    });
+  }
+
+  Future<void> downvotePrompt(String userId, String promptId) async {
+    await _firestore.collection('Users').doc(userId).update({
+      'downvoted_prompts.$promptId': Timestamp.now(),
+    });
+  }
+
+  Future<void> removeUpvote(String userId, String promptId) async {
+    await _firestore.collection('Users').doc(userId).update({
+      'upvoted_prompts.$promptId': FieldValue.delete(),
+    });
+  }
+
+  Future<void> removeDownvote(String userId, String promptId) async {
+    await _firestore.collection('Users').doc(userId).update({
+      'downvoted_prompts.$promptId': FieldValue.delete(),
+    });
+  }
+
+  Future<void> addFavouritePrompt(String userId, String promptId) async {
+    await _firestore.collection('Users').doc(userId).update({
+      'fav_prompts': FieldValue.arrayUnion([promptId]),
+    });
+  }
+
+  Future<void> removeFavouritePrompt(String userId, String promptId) async {
+    await _firestore.collection('Users').doc(userId).update({
+      'fav_prompts': FieldValue.arrayRemove([promptId]),
+    });
+  }
+
+  Future<void> addCreatedPrompt(String userId, String promptId) async {
+    await _firestore.collection('Users').doc(userId).update({
+      'created_prompts': FieldValue.arrayUnion([promptId]),
+    });
+  }
 }
 
 final userServiceProvider = Provider<UserService>((ref) {
