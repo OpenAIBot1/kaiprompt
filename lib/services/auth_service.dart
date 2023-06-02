ac2e90f2472404d63b7fec382b059632f1375b66
Aleksandr Podgaiko <aleksandrpodgaiko@Aleksandrs-MacBook-Pro.local>
2023-06-02 11:22:51 +0800

    added auth service

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,34 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+// Declare the provider
+final authServiceProvider = StateNotifierProvider<AuthService, User?>(
+    (ref) => AuthService(FirebaseAuth.instance));
+
+class AuthService extends StateNotifier<User?> {
+  final FirebaseAuth _firebaseAuth;
+
+  AuthService(this._firebaseAuth) : super(_firebaseAuth.currentUser) {
+    _firebaseAuth.authStateChanges().listen((user) {
+      state = user;
+    });
+  }
+
+  Future<void> signIn(String email, String password) async {
+    await _firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
+  }
+
+  Future<void> signOut() async {
+    await _firebaseAuth.signOut();
+  }
+
+  Future<void> register(String email, String password) async {
+    await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email, password: password);
+  }
+
+  Future<void> resetPassword(String email) async {
+    await _firebaseAuth.sendPasswordResetEmail(email: email);
+  }
+}
